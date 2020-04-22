63e8dacd3fa12c55e9d2220b3ede4673614d8b70
Jeffrey Kazmierski <31972651+starvec@users.noreply.github.com>
2020-04-21 23:59:24 -0700

    started main program initilization window

--- src/net/starvec/InterfaceMainInitilization.java
+++ src/net/starvec/InterfaceMainInitilization.java
@@ -1,12 +1,22 @@
 package net.starvec;
 
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JProgressBar;
+import javax.swing.SpringLayout;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class InterfaceMainInitilization {
 
 	private JFrame frame;
+	
+	private JProgressBar progressBar;
 
 	/**
 	 * Launch the application.
@@ -36,10 +46,45 @@ public class InterfaceMainInitilization {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
+		
+		UIManager.put("ProgressBar.selectionForeground", Color.white);
+		UIManager.put("ProgressBar.selectionBackground", Color.black);
+		
 		frame = new JFrame();
-		frame.setResizable(false);
-		frame.setBounds(100, 100, 450, 300);
+		frame.setUndecorated(true);
+		frame.setBounds(100, 100, 300, 50);
+		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		SpringLayout springLayout = new SpringLayout();
+		frame.getContentPane().setLayout(springLayout);
+		
+		JLabel lbl = new JLabel("Initializing");
+		lbl.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lbl.setHorizontalAlignment(SwingConstants.CENTER);
+		springLayout.putConstraint(SpringLayout.NORTH, lbl, 4, SpringLayout.NORTH, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, lbl, 4, SpringLayout.WEST, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, lbl, -4, SpringLayout.EAST, frame.getContentPane());
+		frame.getContentPane().add(lbl);
+		
+		progressBar = new JProgressBar();
+		progressBar.setStringPainted(true);
+		progressBar.setForeground(new Color(51, 187, 76));
+		springLayout.putConstraint(SpringLayout.NORTH, progressBar, 4, SpringLayout.SOUTH, lbl);
+		springLayout.putConstraint(SpringLayout.WEST, progressBar, 4, SpringLayout.WEST, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, progressBar, -4, SpringLayout.SOUTH, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, progressBar, -4, SpringLayout.EAST, frame.getContentPane());
+		frame.getContentPane().add(progressBar);
 	}
 
 }
