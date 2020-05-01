3e06d5e4ba9e2ace647615d2053c7ef9ec7d4f44
Jeffrey Kazmierski <31972651+starvec@users.noreply.github.com>
2020-05-01 16:54:11 -0700

    corrected issue resulting in missed sensors

--- src/net/starvec/InterfaceMainInitilizationFirstTime.java
+++ src/net/starvec/InterfaceMainInitilizationFirstTime.java
@@ -175,182 +175,216 @@ public class InterfaceMainInitilizationFirstTime
 			{
 				DBAction.executeUpdate(dbConnection, "DELETE FROM sensor_master WHERE 1;");
 				
-				String sensorListRaw = IOUtils.toString(new URL("https://www.purpleair.com/json?show="), Charset.forName("UTF-8"));
+				String sensorListRaw = IOUtils.toString(new URL("https://www.purpleair.com/json?show="), Charset.forName("UTF-8"));	
 				
 				progressBar.setValue(sensorListDownloadCompletedProgressValue);
 				progressBar.setString("Parsing Air Sensor List");
 				
-				//System.out.println(sensorListRaw.length());
-				//7965862
-				//7965736
-				
 				int packedStatements = 0;
 				String insertionStatement = "";
 				
-				for (int i = 0; i < sensorListRaw.length() - 5; i++)
-				{		
-					// scan until we find "ID":
-					if (sensorListRaw.charAt(i) == '\"' && sensorListRaw.charAt(i+1) == 'I' && sensorListRaw.charAt(i+2) == 'D' 
-						&& sensorListRaw.charAt(i+3) == '\"' && sensorListRaw.charAt(i+4) == ':')
-					{	
-						int id;
-						int parentId = -1;
-						boolean isChild;
-						String name;
-						String sensorType;
-						float latitude;
-						float longitude;
-						String locationType;
-						
-						i += 5;
-						int iIdStart = i;								// mark start of id
-						while (sensorListRaw.charAt(i) != ',') {		// scan until ,
-							i++;
-						}
-						int iIdEnd = i;									// mark end of id
-						
-						// parse id
-						id = Integer.parseInt(sensorListRaw.substring(iIdStart, iIdEnd));
-						
-						// if field after id is "ParentID"
-						if (sensorListRaw.charAt(i+2) == 'P')
-						{
-							i += 12;
-							int iParentIdStart = i;						// mark start of parent id
-							while (sensorListRaw.charAt(i) != ',') {	// scan until ,
-								i++;
-							}
-							int iParentIdEnd = i;						// mark end of parent id
-							
-							// parse parent id
-							parentId = Integer.parseInt(sensorListRaw.substring(iParentIdStart, iParentIdEnd));
-							isChild = true;
-						}
-						else {
-							isChild = false;
-						}
-						
-						i += 10;					
-						int iNameStart = i;								// mark start of name
-						while (sensorListRaw.charAt(i) != '\"') {		// scan until "
-							i++;
-						}
-						int iNameEnd = i;								// mark end of name
-
-						// parse name
-						name = sensorListRaw.substring(iNameStart, iNameEnd);
+				// split each sensor into its own string
+				String sensorStrings[] = sensorListRaw.split("\"ID\":");
+				
+				for (int s = 1; s < sensorStrings.length; s++)
+				{
+					String sensorString = sensorStrings[s];
 					
-						// if sensor has a location type
-						if (sensorListRaw.substring(i+3, i+22).equals("DEVICE_LOCATIONTYPE"))
-						{
-							i += 25;
-							int iLocationTypeStart = i;					// mark start of location type
-							while (sensorListRaw.charAt(i) != '\"') {	// scan until "
-								i++;
-							}
-							int iLocationTypeEnd = i;					// mark end of location type
-							
-							// parse location type
-							locationType = sensorListRaw.substring(iLocationTypeStart, iLocationTypeEnd);
-						}
-						// if sensor does not have a location type
-						else
-						{
-							// set location type
-							locationType = "undefined";
-						}
-
-						// scan until "Lat":
-						while (!(sensorListRaw.charAt(i) == '\"' && sensorListRaw.charAt(i+1) == 'L' && sensorListRaw.charAt(i+2) == 'a' 
-								&& sensorListRaw.charAt(i+3) == 't' && sensorListRaw.charAt(i+4) == '\"' && sensorListRaw.charAt(i+5) == ':')) 
-						{
-							i++;
-						}
-
-						i += 6;
-						int iLatStart = i;								// mark start of latitude
-						while (sensorListRaw.charAt(i) != ',') {		// scan until ,
-							i++;
-						}
-						int iLatEnd = i;								// mark end of latitude
-						
-						// parse latitude
-						latitude = Float.parseFloat(sensorListRaw.substring(iLatStart, iLatEnd));
-
-						i += 7;
-						int iLonStart = i;								// mark start of longitude
-						while (sensorListRaw.charAt(i) != ',') {		// scan until ,
-							i++;
-						}
-						int iLonEnd = i;								// mark end of longitude
-						
-						// parse longitude
-						longitude = Float.parseFloat(sensorListRaw.substring(iLonStart, iLonEnd));
-						
-						i += 10;
-						int startingIndex = i;
-						// scan for "Type":
-						while (!(sensorListRaw.charAt(i) == '\"' && sensorListRaw.charAt(i+1) == 'T' && sensorListRaw.charAt(i+2) == 'y' 
-								&& sensorListRaw.charAt(i+3) == 'p' && sensorListRaw.charAt(i+4) == 'e' && sensorListRaw.charAt(i+5) == '\"'
-								&& sensorListRaw.charAt(i+6) == ':') && (i - startingIndex < 50))
-						{
-							i++;
-						}
-						i += 8;
-						int iSensorTypeStart = i;						// mark start of sensor type
-						while (sensorListRaw.charAt(i) != '\"') {		// scan until "
-							i++;
-						}
-						int iSensorTypeEnd = i;							// mark end of sensor type
-						
-						// parse sensor type
-						sensorType = sensorListRaw.substring(iSensorTypeStart, iSensorTypeEnd);
-						
-						if (isChild)
-						{	
-							packedStatements++;
-							insertionStatement += "INSERT INTO sensor_master (" +
-													"sm_sensor_id, " + 
-													"sm_parent_sensor_id, " +
-													"sm_is_child, " + 
-													"sm_sensor_name, " +
-													"sm_latitude, " + 
-													"sm_longitude, " +
-													"sm_location_type" +
-													") " +
-														"VALUES (" +
-															id + ", " +
-															parentId + ", " + 
-															isChild + ", " +
-															"\"" + name + "\", " + 
-															latitude + ", " + 
-															longitude + ", " + 
-															"\"" + locationType + "\");\n";
-						}
-						else
-						{
-							packedStatements++;
-							insertionStatement += "INSERT INTO sensor_master (" +
-													"sm_sensor_id, " +
-													"sm_is_child, " +
-													"sm_sensor_name, " +
-													"sm_sensor_type, " +
-													"sm_latitude, " +
-													"sm_longitude, " +
-													"sm_location_type" +
-													") " +
-														"VALUES (" +
-															id + ", " +
-															isChild + ", " +
-															"\"" + name + "\", " + 
-															"\"" + sensorType + "\", " +
-															latitude + ", " + 
-															longitude + ", " + 
-															"\"" + locationType + "\");\n";
-						}				
+					int id;
+					int parentId;
+					boolean isChild;
+					String name;
+					String sensorType;
+					float latitude;
+					float longitude;
+					boolean hasLocation;
+					String locationType;
+					
+					int start = 0;
+					int end = 0;
+					while (sensorString.charAt(end) != ',') {
+						end++;
 					}
 					
-					// send the multi-line insertion statment to the database, then resets the string to accept more statements
+					// parse id
+					id = Integer.parseInt(sensorString.substring(start, end));
+					
+					// if sensor has parent id
+					if (sensorString.indexOf("\"ParentID\":") != -1)
+					{
+						isChild = true;
+						start = sensorString.indexOf("\"ParentID\":") + 11;
+						end = start;
+						while (sensorString.charAt(end) != ',') {
+							end++;
+						}
+					
+						// parse parent id
+						parentId = Integer.parseInt(sensorString.substring(start, end));
+					}
+					else
+					{
+						isChild = false;
+						parentId = -1;
+					}
+					
+					start = sensorString.indexOf("\"Label\":") + 9;
+					end = start;
+					while (sensorString.charAt(end) != ',') {
+						end++;
+					}
+					
+					// parse name
+					name = sensorString.substring(start, end-1);
+					
+					// if sensor has type
+					if (sensorString.indexOf("\"Type\":") != -1)
+					{
+						start = sensorString.indexOf("\"Type\":") + 8;
+						end = start;
+						while (sensorString.charAt(end) != ',') {
+							end++;
+						}
+						
+						// parse sensor type
+						sensorType = sensorString.substring(start, end-1);
+					}
+					else
+					{
+						sensorType = "undefined";
+					}					
+					
+					// if sensor has location
+					if (sensorString.indexOf("\"Lat\":") != -1)
+					{
+						hasLocation = true;
+						start = sensorString.indexOf("\"Lat\":") + 6;
+						end = start;
+						while (sensorString.charAt(end) != ',') {
+							end++;
+						}	
+						
+						// parse latitude
+						latitude = Float.parseFloat(sensorString.substring(start, end));
+						
+						start = sensorString.indexOf("\"Lon\":") + 6;
+						end = start;
+						while (sensorString.charAt(end) != ',') {
+							end++;
+						}
+						
+						// parse longitude
+						longitude = Float.parseFloat(sensorString.substring(start, end));
+					}
+					else
+					{
+						hasLocation = false;
+						latitude = -1;
+						longitude = -1;
+					}					
+					
+					// if sensor has location type
+					if (sensorString.indexOf("\"DEVICE_LOCATIONTYPE\":") != -1)
+					{
+						start = sensorString.indexOf("\"DEVICE_LOCATIONTYPE\":") + 23;
+						end = start;
+						while (sensorString.charAt(end) != ',') {
+							end++;
+						}
+						
+						// parse location type
+						locationType = sensorString.substring(start, end-1);
+					}
+					else
+					{
+						locationType = "undefined";
+					}
+					
+					// add correct insertion statement to statements list
+					if (isChild && hasLocation)
+					{	
+						packedStatements++;
+						insertionStatement += "INSERT INTO sensor_master (" +
+												"sm_sensor_id, " + 
+												"sm_parent_sensor_id, " +
+												"sm_is_child, " + 
+												"sm_sensor_name, " +
+												"sm_latitude, " + 
+												"sm_longitude, " +
+												"sm_location_type" +
+												") " +
+													"VALUES (" +
+														id + ", " +
+														parentId + ", " + 
+														isChild + ", " +
+														"\"" + name + "\", " + 
+														latitude + ", " + 
+														longitude + ", " + 
+														"\"" + locationType + "\");\n";
+					}
+					else if (!isChild && hasLocation)
+					{
+						packedStatements++;
+						insertionStatement += "INSERT INTO sensor_master (" +
+												"sm_sensor_id, " +
+												"sm_is_child, " +
+												"sm_sensor_name, " +
+												"sm_sensor_type, " +
+												"sm_latitude, " +
+												"sm_longitude, " +
+												"sm_location_type" +
+												") " +
+													"VALUES (" +
+														id + ", " +
+														isChild + ", " +
+														"\"" + name + "\", " + 
+														"\"" + sensorType + "\", " +
+														latitude + ", " + 
+														longitude + ", " + 
+														"\"" + locationType + "\");\n";
+					}
+					else if (isChild && !hasLocation)
+					{
+						packedStatements++;
+						insertionStatement += "INSERT INTO sensor_master (" +
+												"sm_sensor_id, " + 
+												"sm_parent_sensor_id, " +
+												"sm_is_child, " + 
+												"sm_sensor_name, " +
+												"sm_location_type" +
+												") " +
+													"VALUES (" +
+														id + ", " +
+														parentId + ", " + 
+														isChild + ", " +
+														"\"" + name + "\", " + 
+														"\"" + locationType + "\");\n";
+					}
+					else if (!isChild && !hasLocation)
+					{
+						packedStatements++;
+						insertionStatement += "INSERT INTO sensor_master (" +
+												"sm_sensor_id, " +
+												"sm_is_child, " +
+												"sm_sensor_name, " +
+												"sm_sensor_type, " +
+												"sm_location_type" +
+												") " +
+													"VALUES (" +
+														id + ", " +
+														isChild + ", " +
+														"\"" + name + "\", " + 
+														"\"" + sensorType + "\", " +
+														"\"" + locationType + "\");\n";
+					}
+					// should not be able to get here
+					else
+					{
+						System.err.println("Should not be able to get here");
+					}
+				
+				
+					// send the multi-line insertion statement to the database, then reset the string to accept more statements
 					if (packedStatements >= 1000)
 					{
 						DBAction.executeUpdateAsTransaction(dbConnection, insertionStatement);
@@ -359,10 +393,9 @@ public class InterfaceMainInitilizationFirstTime
 					}
 					
 					// update the progress bar as we go
-					if (i%100 == 0)
-						progressBar.setValue((int) (sensorListDownloadCompletedProgressValue + (i/(float)sensorListRaw.length())*(sensorListParseCompletedProgressValue - sensorListDownloadCompletedProgressValue)));
-				
-				} // end for loop
+					if (s%100 == 0)
+						progressBar.setValue((int) (sensorListDownloadCompletedProgressValue + (s/(float)sensorStrings.length)*(sensorListParseCompletedProgressValue - sensorListDownloadCompletedProgressValue)));
+				}
 				
 				// if there are any insertion statements left, send them to the database
 				if (packedStatements > 0)
@@ -403,8 +436,6 @@ public class InterfaceMainInitilizationFirstTime
 				
 				while ((airport = br.readLine()) != null)
 				{
-					//airport = airport.replaceAll("^\"|\"$", "");
-					//String[] fields = airport.replaceAll("\"", "").split(",");
 					String[] fields = new String[100];
 					int currentField = 0;
 					boolean betweenQuotes = false;
@@ -436,11 +467,9 @@ public class InterfaceMainInitilizationFirstTime
 													"\"" + fields[10] + "\");\n";
 						count++;
 						packedStatements++;
-						
-						//System.out.println(Arrays.toString(fields));
 					}
 					
-					// send the multi-line insertion statment to the database, then resets the string to accept more statements
+					// send the multi-line insertion statement to the database, then resets the string to accept more statements
 					if (packedStatements >= 1000)
 					{
 						DBAction.executeUpdateAsTransaction(dbConnection, insertionStatement);
