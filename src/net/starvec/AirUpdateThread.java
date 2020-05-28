3836e5cdbf8897bbcd467feb446266f477dce767
Jeffrey Kazmierski <31972651+starvec@users.noreply.github.com>
2020-05-28 12:50:37 -0700

    added console logging

--- src/net/starvec/AirUpdateThread.java
+++ src/net/starvec/AirUpdateThread.java
@@ -1,6 +1,7 @@
 package net.starvec;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -28,6 +29,7 @@ public class AirUpdateThread extends Thread
 			for (int i = 0; i < sensors.size(); i++)
 			{
 				// refresh the data, handle that refreshed data, and sleep
+				System.out.println("Attempting to get new data for air sensor " + sensors.get(i).getPrimaryName() + "(" + sensors.get(i).getPrimaryId() + ")");
 				sensors.get(i).refreshSensorData();
 				handleSensorRefresh(i);
 				sleep(MIN_TIME_BETWEEN_REFRESH);
@@ -43,56 +45,69 @@ public class AirUpdateThread extends Thread
 		LocalDateTime readingTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(sensor.getLastPrimaryReadingTime()*1000), 
                 TimeZone.getDefault().toZoneId());
 		
-		// insert sensor A data
-		DBAction.executeUpdate(dbConnection,
-				"INSERT INTO air_reading " +
-					"VALUES (" +
-						sensor.getPrimaryId() + ", " +
-						sensor.getPrimaryP0_3Count() + ", " +
-						sensor.getPrimaryP0_5Count() + ", " +
-						sensor.getPrimaryP1Count() + ", " +
-						sensor.getPrimaryP2_5Count() + ", " +
-						sensor.getPrimaryP5Count() + ", " +
-						sensor.getPrimaryP10Count() + ", " +
-						sensor.getPrimaryPm1Value() + ", " +
-						sensor.getPrimaryPm2_5Value() + ", " +
-						sensor.getPrimaryPm10Value() + ", " +
-						sensor.getTemperatureF() + ", " + 
-						sensor.getHumidity() + ", " + 
-						sensor.getPressure() + ", " +
-						"\"" + readingTime + "\"" +
-					");"
-				);
+		try 
+		{
+			// insert sensor A data
+			DBAction.executeUpdateUncaught(dbConnection,
+					"INSERT INTO air_reading " +
+						"VALUES (" +
+							sensor.getPrimaryId() + ", " +
+							sensor.getPrimaryP0_3Count() + ", " +
+							sensor.getPrimaryP0_5Count() + ", " +
+							sensor.getPrimaryP1Count() + ", " +
+							sensor.getPrimaryP2_5Count() + ", " +
+							sensor.getPrimaryP5Count() + ", " +
+							sensor.getPrimaryP10Count() + ", " +
+							sensor.getPrimaryPm1Value() + ", " +
+							sensor.getPrimaryPm2_5Value() + ", " +
+							sensor.getPrimaryPm10Value() + ", " +
+							sensor.getTemperatureF() + ", " + 
+							sensor.getHumidity() + ", " + 
+							sensor.getPressure() + ", " +
+							"\"" + readingTime + "\"" +
+						");"
+					);
 		
-		// insert sensor B data
-		DBAction.executeUpdate(dbConnection,
-				"INSERT INTO air_reading (" +
-						"ar_sensor_id, " +
-						"ar_p0_3_count, " +
-						"ar_p0_5_count, " +
-						"ar_p1_count, " +
-						"ar_p2_5_count, " +
-						"ar_p5_count, " +
-						"ar_p10_count, " +
-						"ar_pm1_value, " +
-						"ar_pm2_5_value, " +
-						"ar_pm10_value, " +
-						"ar_datetime" +
-					") " +
-					"VALUES (" +
-						sensor.getSecondaryId() + ", " +
-						sensor.getSecondaryP0_3Count() + ", " +
-						sensor.getSecondaryP0_5Count() + ", " +
-						sensor.getSecondaryP1Count() + ", " +
-						sensor.getSecondaryP2_5Count() + ", " +
-						sensor.getSecondaryP5Count() + ", " +
-						sensor.getSecondaryP10Count() + ", " +
-						sensor.getSecondaryPm1Value() + ", " +
-						sensor.getSecondaryPm2_5Value() + ", " +
-						sensor.getSecondaryPm10Value() + ", " +
-						"\"" + readingTime + "\"" +
-					");"
-				);
+		
+			// insert sensor B data
+			DBAction.executeUpdateUncaught(dbConnection,
+					"INSERT INTO air_reading (" +
+							"ar_sensor_id, " +
+							"ar_p0_3_count, " +
+							"ar_p0_5_count, " +
+							"ar_p1_count, " +
+							"ar_p2_5_count, " +
+							"ar_p5_count, " +
+							"ar_p10_count, " +
+							"ar_pm1_value, " +
+							"ar_pm2_5_value, " +
+							"ar_pm10_value, " +
+							"ar_datetime" +
+						") " +
+						"VALUES (" +
+							sensor.getSecondaryId() + ", " +
+							sensor.getSecondaryP0_3Count() + ", " +
+							sensor.getSecondaryP0_5Count() + ", " +
+							sensor.getSecondaryP1Count() + ", " +
+							sensor.getSecondaryP2_5Count() + ", " +
+							sensor.getSecondaryP5Count() + ", " +
+							sensor.getSecondaryP10Count() + ", " +
+							sensor.getSecondaryPm1Value() + ", " +
+							sensor.getSecondaryPm2_5Value() + ", " +
+							sensor.getSecondaryPm10Value() + ", " +
+							"\"" + readingTime + "\"" +
+						");"
+					);
+			
+			System.out.println("New data recorded");
+		} 
+		catch (SQLException e) 
+		{
+			if (e.toString().contains("[SQLITE_CONSTRAINT_PRIMARYKEY]"))
+				System.out.println("No new data available");
+			else
+				e.printStackTrace();
+		}
 	}
 	
 	private void sleep(int millis)
